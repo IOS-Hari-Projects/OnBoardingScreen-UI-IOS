d3c23ea56c55dee5e32843863272fc32b5b56806
hari117 <hari.hari.cj@gmail.com>
2024-09-07 11:43:11 +0530

    Initial Commit

---
+++ onBoardingScreen/onBoardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  onBoardingScreenApp.swift
+//  onBoardingScreen
+//
+//  Created by Hari Krishna S on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct onBoardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
